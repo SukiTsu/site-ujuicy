c31d7ea029d538fb546ce9a6885454410773fe86
SukiTsu <buibrandon47@gmail.com>
2025-02-07 12:08:15 +0100

    Modification du style des évènements passé

--- src/page/PageEvenementType.tsx
+++ src/page/PageEvenementType.tsx
@@ -1,4 +1,5 @@
 import "../assets/style/stylePagePrestation.css"
+import "../assets/style/component/stylePasseEvenement.css"
 import "../assets/style/styleIndex.css"
 import NavBarre from "../component/NavBarre";
 
@@ -6,7 +7,6 @@ const PageEvents = ({}) => {
 
     const pastEvents = [
         {
-          id: 1,
           title: "Notre première prestation, à la Paris Manga !",
           date: "12 Juillet 2023",
           description: "En collaboration avec l'AMF Cosplay, nous avons eu l'opportunité d'avoir une petite scène",
@@ -22,16 +22,17 @@ const PageEvents = ({}) => {
       <h2>Nos Prestations Passées</h2>
       <div className="events-grid">
         {pastEvents.map(event => (
-            <div className="event-card">
-                <video autoPlay muted loop>
-                    <source src="./public/video_bk.mp4" type="video/mp4" className="event-video"></source>
+          
+            <div className="event-card" key={event.title}>
+                <video autoPlay muted loop className="event-video">
+                    <source src={event.video} type="video/mp4"></source>
                 </video>
-            <div className="event-info">
-              <h3>{event.title}</h3>
-              <p className="event-date">{event.date}</p>
-              <p className="event-description">{event.description}</p>
+              <div className="event-info">
+                <h3>{event.title}</h3>
+                <p className="event-date">{event.date}</p>
+                <p className="event-description">{event.description}</p>
+              </div>
             </div>
-          </div>
         ))}
       </div>
     </div>
